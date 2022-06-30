b39eae16b6781c19288b6f080b2cceedcc505c24
Flavio Figueiredo <flavio@dcc.ufmg.br>
2022-06-30 15:52:46 -0300

    unit test for kde pdf

--- copulae/tests/test_kde.py
+++ copulae/tests/test_kde.py
@@ -1,11 +1,12 @@
 # -*- coding: utf8 -*-
 '''Unit tests for our kde estimates'''
 
+from copulae.kde import kde_pdf
+
 from numpy.testing import assert_almost_equal
 
 
 import jax
-import jax.numpy as jnp
 
 import scipy.stats as ss
 
@@ -14,8 +15,10 @@ def test_pdf():
     key = jax.random.PRNGKey(30091985)
     _, key = jax.random.split(key)
     data = jax.random.normal(key, shape=(100, ))
-    skde = ss.gaussian_kde(data)
-    pass
+    kde_ss = ss.gaussian_kde(data)
+    y_ss = kde_ss.pdf(data)
+    y = kde_pdf(data)
+    assert_almost_equal(y_ss, y)
 
 
 def test_cdf():
