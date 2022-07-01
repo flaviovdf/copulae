876215707d57af58e16c3d1530142a32300c46a6
Flavio Figueiredo <flavio@dcc.ufmg.br>
2022-07-01 10:41:31 -0300

    test for gauss copula

--- copulae/tests/test_utils.py
+++ copulae/tests/test_utils.py
@@ -2,20 +2,30 @@
 '''Unit tests for the utilities module'''
 
 
-from copulae.utils import ecdf
+from copulae.utils import gauss_copula
 
-from statsmodels.distributions.empirical_distribution \
-    import ECDF
-
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_array_equal
 
 
 import jax.numpy as jnp
+import jax.scipy.stats as jss
 
 
-def test_ecdf():
-    data = jnp.array([2, 1, 4, 0, 5, 6, 7, 1, 2, 4, 5, 2])
-    ecdf_y = ecdf(data)
+def test_gausscopula():
+    rho = 0
+    mean = jnp.zeros(2)
+    mean[1] = 2
 
-    ecdf_sm = ECDF(data)
-    assert_almost_equal(ecdf_sm(data), ecdf_y)
+    E = jnp.zeros(shape=(2, 2)) + rho
+    E = E.at[0, 0].set(1)
+    E = E.at[1, 1].set(1)
+
+    u = jnp.array([0.25, 0.5, 0.75])
+    x1 = jss.norm.ppf(u, loc=0, scale=1)
+    x2 = jss.norm.ppf(u, loc=2, scale=1)
+
+    cdf1 = jss.norm.cdf(x1, loc=0, scale=1)
+    cdf2 = jss.norm.cdf(x2, loc=0, scale=1)
+
+    assert_array_equal(cdf1 * cdf2,
+                       gauss_copula(u, mean, E))
--- copulae/utils.py
+++ copulae/utils.py
@@ -3,44 +3,8 @@
 
 
 from copulae.typing import Tensor
-from copulae.typing import PyTree
-
-import jax
-import jax.numpy as jnp
 
 import scipy.stats as ss
-
-
-@jax.jit
-def ecdf(data: Tensor) -> PyTree:
-    '''
-    Returns an empirical cumulative distribution estimate
-    for the given datapoints. Return values are not
-    sorted. In order to plot one should do as follows:
-
-    >>> from copulate.utils import ecdf
-    >>> import jax.numpy as jnp
-    >>> data = jnp.array([3.0, 1.0, 1.0, 7.0, -2.2])
-    >>> ecdf_y = ecdf(data)
-    >>> asort = data.argsort()
-    >>> x_plot = data[asort]
-    >>> y_plot = data[asort]
-    >>> # from here you can plot x and y
-
-    Arguments
-    ---------
-    data: Tensor (1 dimensional array-like)
-        the data set to compute the ecdf
-
-    Returns
-    -------
-    y: Tensor (1 dimensional jnp.array)
-        the ecdf estimate for give datapoints
-    '''
-    xs = jnp.sort(data)
-    n = xs.shape[0]
-    y = (jnp.searchsorted(xs, data, side="right") - 1) / n
-    return y
 
 
 def gauss_copula(
