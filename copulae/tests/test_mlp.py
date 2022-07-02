82e63820ca8b0f1d2fb465e69749c556862129fd
Flavio Figueiredo <flavio@dcc.ufmg.br>
2022-07-02 09:44:42 -0300

    new tests for mlp

--- copulae/tests/test_mlp.py
+++ copulae/tests/test_mlp.py
@@ -5,7 +5,6 @@
 from copulae.mlp import init_mlp
 
 from numpy.testing import assert_equal
-from numpy.testing import assert_true
 
 
 import jax
@@ -17,24 +16,24 @@ def test_init_mlp():
     assert_equal(5, len(params))
 
     weights, bias = params[0]
-    assert_true((weights != 0).any())
-    assert_true(weights.shape[0] == 8)
-    assert_true(weights.shape[1] == 3)
-    assert_true((bias == 1).all())
+    assert((weights != 0).any())
+    assert(weights.shape[0] == 8)
+    assert(weights.shape[1] == 3)
+    assert((bias == 1).all())
 
     for weights, bias in params[1:-1]:
-        assert_true((weights != 0).any())
-        assert_true(weights.shape[0] == 8)
-        assert_true(weights.shape[1] == 8)
-        assert_true((bias == 1).all())
+        assert((weights != 0).any())
+        assert(weights.shape[0] == 8)
+        assert(weights.shape[1] == 8)
+        assert((bias == 1).all())
 
     weights, bias = params[-1]
-    assert_true((weights != 0).any())
-    assert_true(weights.shape[0] == 1)
-    assert_true(weights.shape[1] == 8)
-    assert_true((bias == 1).all())
+    assert((weights != 0).any())
+    assert(weights.shape[0] == 1)
+    assert(weights.shape[1] == 8)
+    assert((bias == 1).all())
 
-    assert_true(new_key != key)
+    assert(new_key != key)
 
 def test_init_mlp_2():
     key = jax.random.PRNGKey(30091985)
@@ -42,21 +41,21 @@ def test_init_mlp_2():
     assert_equal(2, len(params))
 
     weights, bias = params[0]
-    assert_true((weights != 0).any())
-    assert_true(weights.shape[0] == 4)
-    assert_true(weights.shape[1] == 2)
-    assert_true((bias == 1).all())
+    assert((weights != 0).any())
+    assert(weights.shape[0] == 4)
+    assert(weights.shape[1] == 2)
+    assert((bias == 1).all())
 
     for weights, bias in params[1:-1]:
-        assert_true((weights != 0).any())
-        assert_true(weights.shape[0] == 4)
-        assert_true(weights.shape[1] == 4)
-        assert_true((bias == 0).all())
+        assert((weights != 0).any())
+        assert(weights.shape[0] == 4)
+        assert(weights.shape[1] == 4)
+        assert((bias == 0).all())
 
     weights, bias = params[-1]
-    assert_true((weights != 0).any())
-    assert_true(weights.shape[0] == 1)
-    assert_true(weights.shape[1] == 4)
-    assert_true((bias == 0).all())
+    assert((weights != 0).any())
+    assert(weights.shape[0] == 1)
+    assert(weights.shape[1] == 4)
+    assert((bias == 0).all())
 
-    assert_true(new_key != key)
+    assert(new_key != key)
