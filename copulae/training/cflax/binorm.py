049553c50ac0334b0ff26463f8ab90016f7d4fec
Flavio Figueiredo <social@flaviovdf.io>
2023-05-07 10:18:05 -0300

    added constat to avoid division by zero

--- copulae/training/cflax/binorm.py
+++ copulae/training/cflax/binorm.py
@@ -180,21 +180,13 @@ class NormalBi(nn.Module):
             jax.nn.initializers.constant(1.0),
             (1, 1)
         )[0, 0]
+
         # deviations are always positive
-        s0 = jnp.sqrt(s0 * s0)
-        s1 = jnp.sqrt(s1 * s1)
-        # mu0 = jnp.mean(z0)
-        # s0 = jnp.std(z0, ddof=1)
-
-        # mu1 = jnp.mean(z1)
-        # s1 = jnp.std(z1, ddof=1)
-
-        # rho = jnp.corrcoef(z0, z1)[0, 1]
+        s0 = jnp.sqrt(s0 * s0) + 1e-6
+        s1 = jnp.sqrt(s1 * s1) + 1e-6
 
         p = (z0 - m0) / s0
         q = (z1 - m1) / s1
-        # p = z0
-        # q = z1
 
         rho_x = self.param(
             'rho_x',
@@ -202,7 +194,6 @@ class NormalBi(nn.Module):
             (1, 1)
         )[0, 0]
         rho = jnp.clip(jax.nn.tanh(rho_x), -0.9999, 0.9999)
-        print(rho)
 
         return vbinorm(p, q, rho)
 
