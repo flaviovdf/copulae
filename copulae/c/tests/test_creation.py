da95498fc8fc7771ecf9e1811c50900d127e2d1c
Flavio Figueiredo <flaviovdf@dcc.ufmg.br>
2023-07-17 14:19:39 -0300

    Created tests for second derivative

--- copulae/c/tests/test_creation.py
+++ copulae/c/tests/test_creation.py
@@ -140,3 +140,86 @@ def test_density_shape():
     U = jnp.ones(shape=(6, 2, 12), dtype=jnp.float32)
 
     assert_equal((6, 12), density(params, U).shape)
+
+
+def test_closed_form_density():
+    '''
+    Copulas of the form (FGM copulas):
+
+    C(u, v) = u * v + theta * u * v * (1 - u) * (1 - v)
+
+    Have a closed form density of:
+
+    c(u, v) = 1 + theta * (1 - 2 * u) * (1 - 2 * v)
+
+    See:
+    https://hal.science/hal-00861234/document
+
+    '''
+    @jax.jit
+    def forward_fun(theta, U):
+        theta = theta[0][0]
+        u = U[0]
+        v = U[1]
+        return u * v + theta * u * v * (1 - u) * (1 - v)
+
+    _, _, density = create_copula(forward_fun)
+
+    U = jnp.zeros(shape=(1, 2, 2), dtype=jnp.float32)
+
+    u = 0.6
+    v = 0.2
+    theta = 0.5
+
+    U = U.at[0, 0, 0].set(u)
+    U = U.at[0, 0, 1].set(u)
+
+    params = [jnp.array([theta])]
+
+    assert_almost_equal(
+        density(params, U)[0, 0],
+        1 + theta * (1 - 2 * u) * (1 - 2 * v)
+    )
+
+
+def test_closed_form_density():
+    '''
+    Copulas of the form (FGM copulas):
+
+    C(u, v) = u * v + theta * u * v * (1 - u) * (1 - v)
+
+    Have a closed form density of:
+
+    c(u, v) = 1 + theta * (1 - 2 * u) * (1 - 2 * v)
+
+    See:
+    https://hal.science/hal-00861234/document
+
+    '''
+    @jax.jit
+    def forward_fun(theta, U):
+        theta = theta[0][0]
+        u = U[0]
+        v = U[1]
+        return u * v + theta * u * v * (1 - u) * (1 - v)
+
+    _, _, density = create_copula(forward_fun)
+
+    U = jnp.zeros(shape=(1, 2, 2), dtype=jnp.float32)
+
+    U = U.at[0, 0, 0].set(0.1)
+    U = U.at[0, 1, 0].set(0.2)
+    U = U.at[0, 0, 1].set(0.5)
+    U = U.at[0, 1, 1].set(0.5)
+
+    params = [jnp.array([-0.8])]
+
+    assert_almost_equal(
+        density(params, U)[0, 0],
+        1 - 0.8 * (1 - 2 * 0.1) * (1 - 2 * 0.2)
+    )
+
+    assert_almost_equal(
+        density(params, U)[0, 1],
+        1 - 0.8 * (1 - 2 * 0.5) * (1 - 2 * 0.5)
+    )
