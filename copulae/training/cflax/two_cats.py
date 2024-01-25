2f6947b05e2a67cf9d80ebea44aa316b419c58d2
Flavio Vinicius Diniz de Figueiredo <flaviovdf@greencow.dcc.ufmg.br>
2024-01-25 14:54:58 -0300

    New twocats

---
+++ copulae/training/cflax/two_cats.py
@@ -0,0 +1,41 @@
+# -*- coding: utf8
+
+
+from copulae.training.cflax.binorm import NormalBi
+from copulae.training.cflax.mlp import MLP
+
+from copulae.typing import Tensor
+
+
+from jax.scipy.special import erf
+
+
+import flax.linen as nn
+
+import jax.numpy as jnp
+import jax.scipy.stats as jss
+import jax
+
+@jax.jit
+def erfp(x: Tensor) -> Tensor:
+    return (erf(x) + 1) / 2
+
+
+@jax.jit
+def erfp_integral(x: Tensor) -> Tensor:
+    return 0.5 * (x * erf(x) + (((-x) ** 2) / jnp.pi) + x)
+
+class TwoCats(nn.Module):
+    base: MLP
+
+    @nn.compact
+    def __call__(self, U: Tensor) -> Tensor:
+        z = erfp(self.base(U).ravel())
+        s = erfp_integral(1)
+
+        t_0 = (erfp_integral(z) * U[0]) / s
+        t_1 = (erfp_integral(z) * U[1]) / s
+        x0 = jss.norm.ppf(t_0)
+        x1 = jss.norm.ppf(t_1)
+
+        return NormalBi()(t_0, t_1)
